63f0fe2756e4ea6c5a9cc4c8be65975fba0a537e
Harshit Masiwal <147537521+harshitmasiwal@users.noreply.github.com>
2024-09-16 21:38:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const BASE_URL = "https://api.fxratesapi.com/convert?from=USD&to=INR&date=2012-06-24&amount=234.12&format=json";
 
 var utc = new Date().toJSON().slice(0, 10).replace(/-/g, '-');
-// document.write(utc);
-// console.log(utc);
 
-// for (code in countryList){
-//     console.log(code,countryList[code]);
-// }
 
 const dropdownoptions = document.querySelectorAll(".dropdown select");
 const sumbitBtn = document.querySelector(".sumbit");
@@ -16,36 +11,41 @@ const msg = document.querySelector(".msg");
 
 
 for (let select of dropdownoptions) {
-    for (let countrycode in countryList) {
-        let newoption = document.createElement("option");
-        newoption.innerText = countrycode; // The currency code
-        newoption.value = countrycode; // Value of the option
+    for (let currencyCode in countryList) {
+        let newOption = document.createElement("option");
+        
+        // Create descriptive text (currency code + country name)
+        let countryDetails = countryList[currencyCode];
+        newOption.innerText = `${currencyCode} - ${countryDetails.country}`; // Display as "USD - United States"
+        newOption.value = currencyCode; // Keep the value as currency code (for API compatibility)
 
-        if (select.name === "from" && countrycode === "USD") {
-            newoption.selected = "selected";
+        // Pre-select USD for 'from' and INR for 'to'
+        if (select.name === "from" && currencyCode === "USD") {
+            newOption.selected = "selected";
         }
-        else if (select.name === "to" && countrycode === "INR") {
-            newoption.selected = "selected";
+        else if (select.name === "to" && currencyCode === "INR") {
+            newOption.selected = "selected";
         }
+
         // Append the new option to the current select dropdown
-        select.append(newoption);
+        select.append(newOption);
     }
 
     select.addEventListener("change", (event) => {
         flagUpdater(event.target);
-    })
-}
+    });
+};
 
 const flagUpdater = (element) => {
     let currencycode = element.value;
     // console.log(currencycode);
-    let countrycode = countryList[currencycode];
+    let countrycode = countryList[currencycode]?.code;
     // console.log(countrycode)
 
     let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`;
     let img = element.parentElement.querySelector("img");
     img.src = newsrc;
-}
+};
 
 sumbitBtn.addEventListener("click", async (event) => {
     event.preventDefault();
@@ -66,9 +66,8 @@ sumbitBtn.addEventListener("click", async (event) => {
     
     let convertedAmt = data.result;
     
-    let messege = msg.innerText = `${amtVal} ${fromcurr.value} = ${convertedAmt} ${tocurr.value}`;
-    
 
+    let messege = msg.innerText = `${amtVal} ${fromcurr.value} = ${convertedAmt} ${tocurr.value}`;
 
 });
 
